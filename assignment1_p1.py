2bf740e8a193fb53d9d0c3fd0f611d6940eaed0e
ymno1129 <ymno1129@gmail.com>
2016-04-04 19:17:52 -0700

    BFS part of PA1

---
+++ assignment1_p1.py
@@ -0,0 +1,119 @@
+import sys
+import math
+import queue
+
+class Node:
+    state = None
+    parent = None
+
+    def __init__(self, state):
+        self.state = state
+
+#hash table holding prime numbers(int)
+table = {}
+solvable = False
+
+def isPrime(n):
+    if n % 2 == 0 and n > 2:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def getPossibleNext(current):
+    currNum = current.state
+    next_list = []
+    length = len(str(currNum))
+    
+    #manipulate every digit
+    for x in range(length, 0, -1):
+        offset = math.pow(10, x - 1)
+        
+        currDigit = int(str(currNum)[length - x])
+
+        # subtract offset * currDigit from currNum
+        # for example, if currNum is 23, then when
+        # manipulating the first digit, subtract it
+        # by 20 and start with 3.
+        # when manipulating the second digit, subtract
+        # int by 3 and start with 20.
+        tmpNum = currNum - (currDigit * offset)
+        
+        #for each digit, ten possible variations
+        for y in range(0, 10):
+           next = tmpNum + y * offset
+           
+           if (next in table):
+               continue
+
+           table[next] = 1
+            
+           #discard the number starts with 0
+           if len(str(int(next)))==length:
+               #check if the number is a prime
+               if isPrime(int(next)):
+                   #add the number to list
+                   tmpNode = Node(int(next))
+                   tmpNode.parent = current
+                   next_list.append(tmpNode)
+
+    return next_list
+
+def BFS(start, target):
+    q = queue.Queue(0)
+    q.put(start)
+
+    while not q.empty():
+        tmpNode = q.get()
+        tmpList = getPossibleNext(tmpNode)      
+       
+        for x in tmpList:
+            if (x.state == target):
+                global solvable
+                solvable = True
+                return x
+            q.put(x)
+
+    print("UNSOLVABLE")
+    return
+
+def main():
+    primes = str(sys.stdin.readline()).split()
+    startPrime = int(primes[0])
+    endPrime = int(primes[1])
+    
+    root = Node(startPrime)
+    table[startPrime] = 1
+
+
+    result = BFS(root, endPrime)
+
+    if (solvable):
+        stack = list()
+        while (result != None):
+            stack.append(result.state)
+            result = result.parent 
+        while (stack):
+            print(stack.pop(), end=" ")
+    
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
